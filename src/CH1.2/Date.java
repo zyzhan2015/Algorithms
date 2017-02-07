bd19407a0ac3c75ffe75d6a453eae8effc3eff7c
Zhiying Zhan <zyzhan2015@163.com>
2017-02-07 17:58:01 +0800

    first commit

---
+++ src/CH1.2/Date.java
@@ -0,0 +1,33 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Date {
+	private final int value;
+
+	public Date(int m, int d, int y) {
+		value = y*512 + m*32 + d;
+	}
+
+	public int month() {
+		return (value/32) % 16;
+	}
+
+	public int day() {
+		return value % 32;
+	}
+
+	public int year() {
+		return value / 512;
+	}
+
+	public String toString() {
+		return month() + "/" + day() + "/" + year();
+	}
+
+	public static void main(String[] args) {
+		int m = Integer.parseInt(args[0]);
+		int d = Integer.parseInt(args[1]);
+		int y = Integer.parseInt(args[2]);
+		Date date = new Date(m, d, y);
+		StdOut.println(date);
+	}
+}

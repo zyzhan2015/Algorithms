0612388d19d83aa5ffcda2b2ea1390a2a1effab9
Zhiying Zhan <zyzhan2015@163.com>
2017-02-13 17:50:26 +0800

    add CH1.3

---
+++ src/CH1.3/Reverse.java
@@ -0,0 +1,11 @@
+public class Reverse {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();	
+		while(!StdIn.isEmpty()) {
+			stack.push(StdIn.readInt());
+		}
+		for (int i : stack) {
+			StdOut.println(i);
+		}
+	}
+}

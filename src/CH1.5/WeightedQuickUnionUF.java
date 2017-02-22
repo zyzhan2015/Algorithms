8cebdb6fad50bc0731b6add135c3a1b505ef9255
Zhiying Zhan <zyzhan2015@163.com>
2017-02-22 13:34:06 +0800

    add all

---
+++ src/CH1.5/WeightedQuickUnionUF.java
@@ -0,0 +1,67 @@
+import edu.princeton.cs.algs4.*;
+
+public class WeightedQuickUnionUF {
+    private int[] id;
+    private int[] sz;
+    private int count;
+
+    public WeightedQuickUnionUF(int N) {
+        count = N;
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            sz[i] = 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) {
+            return;
+        }
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+        count--;
+    }
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(N);
+        Stopwatch timer = new Stopwatch();
+        while(!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) {
+                continue;
+            }
+            uf.union(p, q);
+            //StdOut.printf("%6d %6d\n", p, q);
+        }
+        StdOut.println(timer.elapsedTime() + " seconds");
+        StdOut.println(uf.count() + " components");
+    }
+}

3dc0049ac3edbcbac4492579958ec9638c10fd4e
Zhiying Zhan <zyzhan2015@163.com>
2017-03-06 18:03:44 +0800

    addaaddaa

---
+++ src/CH2.1/Merge.java
@@ -0,0 +1,39 @@
+public class Merge {
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid+1, hi);
+        merge(a, lo, mid, hi);
+    }
+    
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                //如果左半边用尽，取右半边元素
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                //如果右半边用尽，取左半边元素
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+}
